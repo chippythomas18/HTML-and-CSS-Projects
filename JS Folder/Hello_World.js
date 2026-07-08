7a7a2b7761929c820db87be8aada6927961cb4e4
Chippy Thomas <chippythomas18@users.noreply.github.com>
2026-07-08 14:30:38 -0400

    Added Favicon

---
+++ JS Folder/Hello_World.js
@@ -0,0 +1,8 @@
+var greetings = 'Hello, World!'
+console.log(greetings)
+
+var greetings1 = 'Welcome to JS!'
+console.log(greetings1)
+
+var greetings2 = 'This is a JS file'
+console.log(greetings2)
